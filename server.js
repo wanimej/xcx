81ab04ba725ad4b94d6d6a2e42d033be07d1f3d0
wanimej <wan@imej.com.my>
2026-02-01 06:00:05 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, 'index.html');
+  
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(500);
+      res.end('Error loading page');
+      return;
+    }
+    
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(content);
+  });
+});
+
+server.listen(5000, '0.0.0.0', () => {
+  console.log('Server running at http://0.0.0.0:5000');
+});
